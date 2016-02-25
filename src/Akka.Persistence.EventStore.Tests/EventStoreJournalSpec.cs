68389f64f70c9cb881d0a1876b51ed25c71ecff3
Vlad Kosarev <vlad@webmustflow.com>
2016-02-25 13:11:48 -0500

    Functionality+tests

---
+++ src/Akka.Persistence.EventStore.Tests/EventStoreJournalSpec.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TestKit.Journal;
+using EventStore.ClientAPI.Embedded;
+using EventStore.Core;
+using System.Net;
+
+namespace Akka.Persistence.EventStore.Tests
+{
+    public class EventStoreJournalSpec : JournalSpec
+    {
+        private static readonly Config SpecConfig = ConfigurationFactory.ParseString(@"
+        akka.persistence {
+            publish-plugin-commands = on
+
+            journal {
+                plugin = ""akka.persistence.journal.eventstore""
+                eventstore {
+                    class = ""Akka.Persistence.EventStore.Journal.EventStoreJournal, Akka.Persistence.EventStore""
+                    plugin-dispatcher = ""akka.actor.default-dispatcher""
+                    connection-string = ""ConnectTo=tcp://admin:changeit@127.0.0.1:4567;""                    
+                }
+            }
+        }");
+
+        private static ClusterVNode Node;
+
+        public EventStoreJournalSpec()
+            : base(SpecConfig, "EventStoreJournalSpec")
+        {
+            Node = EmbeddedVNodeBuilder
+                .AsSingleNode()
+                .RunInMemory()
+                .WithInternalTcpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4566))
+                .WithExternalTcpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4567))
+                .WithInternalHttpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5566))
+                .WithExternalHttpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5567))
+                .Build();
+            Node.Start();
+            Initialize();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            Node.Stop();
+        }
+    }
+
+}
---
+++ src/Akka.Persistence.EventStore.Tests/EventStoreSnapshotSpec.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Configuration;
+using Akka.Persistence.TestKit.Snapshot;
+using EventStore.ClientAPI.Embedded;
+using EventStore.Core;
+using System.Net;
+
+namespace Akka.Persistence.EventStore.Tests
+{
+    public class EventStoreSnapshotSpec : SnapshotStoreSpec
+    {
+        private static readonly Config SpecConfig = ConfigurationFactory.ParseString(@"
+        akka.persistence {
+            publish-plugin-commands = on
+
+            snapshot-store {
+                plugin = ""akka.persistence.snapshot-store.eventstore""
+                eventstore {
+                    class = ""Akka.Persistence.Eventstore.Snapshot.EventStoreSnapshotStore, Akka.Persistence.EventStore""
+                    plugin-dispatcher = ""akka.actor.default-dispatcher""
+                    connection-string = ""ConnectTo=tcp://admin:changeit@127.0.0.1:4567;""                    
+                }
+            }
+        }");
+
+        private static ClusterVNode Node;
+
+        public EventStoreSnapshotSpec()
+            : base(SpecConfig, "EventStoreSnapshotSpec")
+        {
+            Node = EmbeddedVNodeBuilder
+                .AsSingleNode()
+                .RunInMemory()
+                .WithInternalTcpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4566))
+                .WithExternalTcpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4567))
+                .WithInternalHttpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5566))
+                .WithExternalHttpOn(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5567))
+                .Build();
+            Node.Start();
+            Initialize();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            Node.Stop();
+        }
+    }
+
+}
